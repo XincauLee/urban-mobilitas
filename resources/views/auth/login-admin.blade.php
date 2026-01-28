a6a2395f1b0f5c5c1515b6adc14389464a74b23f
mohdzackysyah <mohdzackysyah05@gmail.com>
2026-01-28 22:19:59 +0700

    favicon-login

--- resources/views/auth/login-admin.blade.php
+++ resources/views/auth/login-admin.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login Administrator - PT. Urban Mobilitas Indragiri</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
 </head>
